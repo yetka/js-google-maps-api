760510abc9b4bd8610b872e5c9d9a3c8013149c4
Guest User <Guest@epicodus-7.local>
2018-01-10 15:26:45 -0800

    move location function to back end file

--- js/place-interface.js
+++ js/place-interface.js
@@ -1,5 +1,6 @@
 var apiKey = require('./../.env').apiKey;
 import { Place } from './../js/place.js';
+import { location } from './../js/place.js';
 
 $(document).ready(function() {
   $("#coordinates-form").submit(function(event) {
@@ -8,21 +9,6 @@ $(document).ready(function() {
     let latitude = parseInt($("#latitude").val());
     let longitude = parseInt($("#longitude").val());
     let newPlace = new Place(latitude, longitude);
-
-    function location(place) {
-      var userLatLng = new google.maps.LatLng(place.latitude, place.longitude);
-
-      var myOptions = {
-        zoom : 16,
-        center : userLatLng,
-        mapTypeId : google.maps.MapTypeId.ROADMAP
-      };
-      var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
-      new google.maps.Marker({
-        map: mapObject,
-        position: userLatLng
-      });
-    }
 
     location(newPlace);
   });
--- js/place.js
+++ js/place.js
@@ -4,3 +4,18 @@ export class Place {
     this.longitude = longitude;
   }
 }
+
+export function location(place) {
+  var userLatLng = new google.maps.LatLng(place.latitude, place.longitude);
+
+  var myOptions = {
+    zoom : 5,
+    center : userLatLng,
+    mapTypeId : google.maps.MapTypeId.ROADMAP
+  };
+  var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+  new google.maps.Marker({
+    map: mapObject,
+    position: userLatLng
+  });
+}
