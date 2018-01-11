246d31c7d1fbd06332a7bafa5490c545bf1ac2ef
Guest User <Guest@epicodus-7.local>
2018-01-10 16:25:02 -0800

    fix the order in html header to place styles.css before script lins to a map

--- js/place-interface.js
+++ js/place-interface.js
@@ -1,6 +1,4 @@
-var apiKey = require('./../.env').apiKey;
 import { Place } from './../js/place.js';
-import { location } from './../js/place.js';
 
 $(document).ready(function() {
   $("#coordinates-form").submit(function(event) {
@@ -10,6 +8,22 @@ $(document).ready(function() {
     let longitude = parseInt($("#longitude").val());
     let newPlace = new Place(latitude, longitude);
 
+    function location(place) {
+      var userLatLng = new google.maps.LatLng(place.latitude, place.longitude);
+
+      var myOptions = {
+        zoom : 12,
+        center : userLatLng,
+        mapTypeId : google.maps.MapTypeId.ROADMAP
+      };
+      var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+      console.log(mapObject);
+      new google.maps.Marker({
+        map: mapObject,
+        position: userLatLng
+      });
+    }
+    console.log(newPlace);
     location(newPlace);
   });
 });
