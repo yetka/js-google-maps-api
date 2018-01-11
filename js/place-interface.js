abd4fe598d6af3b007d633900df97bb90ccb910b
Guest User <Guest@epicodus-7.local>
2018-01-10 16:28:20 -0800

    move location function to back end file again

--- js/place-interface.js
+++ js/place-interface.js
@@ -1,4 +1,6 @@
 import { Place } from './../js/place.js';
+import { location } from './../js/place.js';
+
 
 $(document).ready(function() {
   $("#coordinates-form").submit(function(event) {
@@ -8,22 +10,8 @@ $(document).ready(function() {
     let longitude = parseInt($("#longitude").val());
     let newPlace = new Place(latitude, longitude);
 
-    function location(place) {
-      var userLatLng = new google.maps.LatLng(place.latitude, place.longitude);
 
-      var myOptions = {
-        zoom : 12,
-        center : userLatLng,
-        mapTypeId : google.maps.MapTypeId.ROADMAP
-      };
-      var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
-      console.log(mapObject);
-      new google.maps.Marker({
-        map: mapObject,
-        position: userLatLng
-      });
-    }
-    console.log(newPlace);
+
     location(newPlace);
   });
 });
